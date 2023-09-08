ef0dc220eb62519af9fa73229298d74f007c4f1b
Leo Romanovsky <leoromanovsky@users.noreply.github.com>
2023-09-08 15:55:02 -0700

    Add support for typed variations and updated experiment key FF-852 (#9) (#2)
    * Add support for typed variations and updated experiment key FF-852 (#9)
    
    * WIP
    
    * existing tests passing
    
    * use new experiment key
    
    * run tests for pull requests
    
    * update action to fire on push too
    
    * specify main for push
    
    * fix spacing
    
    * try githubs action
    
    * move workflow to top level
    
    * update composer.lock
    
    * remove versio from composer.json
    
    * better compose.lock
    
    * Update composer.lock
    
    ---------
    
    Co-authored-by: Aaron Silverman <aaron.silverman@geteppo.com>

--- src/Logger/LoggerInterface.php
+++ src/Logger/LoggerInterface.php
@@ -5,23 +5,17 @@ namespace Eppo\Logger;
 interface LoggerInterface
 {
     /**
-     * Method used by EppoClient to log assignments.
-     * Will pass to this method all the assignment information, and, based on implementation,
+     * Method used by EppoClient to log assignments to the data warehouse.
+     * This method will be passed all the assignment information, and, based on implementation,
      * try to log this information.
-     *
-     * @param string $experiment
-     * @param string $variation
-     * @param string $subject
-     * @param string $timestamp
-     * @param array $subjectAttributes
-     *
-     * @return void
      */
     public function logAssignment(
         string $experiment,
         string $variation,
         string $subject,
         string $timestamp,
-        array $subjectAttributes = []
+        array $subjectAttributes = [],
+        ?string $allocation = null,
+        ?string $featureFlag = null
     ): void;
 }
