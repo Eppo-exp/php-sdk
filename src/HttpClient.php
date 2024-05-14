357346a10a4ed71ea2966a15625f5dfd0920b831
Ty Potter <tyler@fluxon.com>
2024-05-14 13:54:41 -0600

    readd httpclient for history

---
+++ src/HttpClient.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Eppo;
+
+use Eppo\Config\HardCodedSDKData;
+use Eppo\Config\SDKData;
+use Eppo\Exception\HttpRequestException;
+use Teapot\StatusCode;
+
+class HttpClient
+{
+    /** @var int */
+    const REQUEST_TIMEOUT = 5;
+
+    /** @var string */
+    protected $baseUrl;
+
+    /** @var bool */
+    public $isUnauthorized = false;
+
+    /** @var array */
+    private $sdkParams = [];
+
+    /**
+     * @param string $baseUrl
+     * @param string $apiKey
+     * @param SDKData $SDKData
+     */
+    public function __construct(string $baseUrl, string $apiKey, array $extraQueryParams)
+    {
+        if (!$baseUrl) {
+            $baseUrl = 'https://fscdn.eppo.cloud';
+        }
+        $this->baseUrl = $baseUrl;
+
+        $this->sdkParams = array_merge($extraQueryParams, ['apiKey' => $apiKey]);
+    }
+
+    /**
+     * @param $resource
+     *
+     * @return string
+     *
+     * @throws HttpRequestException
+     */
+    public function get($resource): string
+    {
+        $ch = curl_init();
+
+        // Prepare the URL with query params
+        $url = $this->baseUrl . '/' . ltrim($resource, '/') . '?' . http_build_query($this->sdkParams);
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, self::REQUEST_TIMEOUT);
+        
+        $output = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            throw new HttpRequestException(curl_error($ch), curl_errno($ch));
+        }
+
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($status >= 400) {
+            $this->handleHttpError($status, $output);
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param int $status
+     * @param string $error
+     *
+     * @throws HttpRequestException
+     */
+    private function handleHttpError(int $status, string $error)
+    {
+        $this->isUnauthorized = $status === 401;
+        $isRecoverable = $this->isHttpErrorRecoverable($status);
+        throw new HttpRequestException($error, $status, $isRecoverable);
+    }
+
+    /**
+     * @param int $status
+     *
+     * @return bool
+     */
+    private function isHttpErrorRecoverable(int $status): bool
+    {
+        if ($status >= StatusCode::BAD_REQUEST && $status < StatusCode::INTERNAL_SERVER_ERROR) {
+            return $status === StatusCode::CONFLICT || $status === StatusCode::REQUEST_TIMEOUT;
+        }
+        return true;
+    }
+}
