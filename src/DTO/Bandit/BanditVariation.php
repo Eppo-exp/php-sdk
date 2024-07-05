b75879ab10f8ed7a96db7082abca6a2feb95f3e8
Ty Potter <tyler@fluxon.com>
2024-07-05 12:31:35 -0600

    finetune

--- src/DTO/Bandit/BanditVariation.php
+++ src/DTO/Bandit/BanditVariation.php
@@ -4,17 +4,12 @@ namespace Eppo\DTO\Bandit;
 
 class BanditVariation
 {
-    public string $key;
-    public string $flagKey;
-    public string $variationKey;
-    public string $variationValue;
-
-    public function __construct(string $key, string $flagKey, string $variationKey, string $variationValue)
-    {
-        $this->key = $key;
-        $this->flagKey = $flagKey;
-        $this->variationKey = $variationKey;
-        $this->variationValue = $variationValue;
+    public function __construct(
+        public readonly string $key,
+        public readonly string $flagKey,
+        public readonly string $variationKey,
+        public readonly string $variationValue
+    ) {
     }
 
     public static function fromJson($json): BanditVariation
