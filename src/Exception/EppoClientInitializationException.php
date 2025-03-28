7e0c0590f4b9cac2ea611709a16f0dc82cb680b6
Tyler Potter <tyler.potter@geteppo.com>
2025-03-28 12:07:44 -0600

    fix:better error messaging (#45)
    * fix: better error messaging

--- src/Exception/EppoClientInitializationException.php
+++ src/Exception/EppoClientInitializationException.php
@@ -2,6 +2,15 @@
 
 namespace Eppo\Exception;
 
+use Throwable;
+
 class EppoClientInitializationException extends EppoClientException
 {
+    public function __construct(
+        string $message = "Client Initialization Failed",
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/Exception/InvalidApiKeyException.php
+++ src/Exception/InvalidApiKeyException.php
@@ -2,6 +2,15 @@
 
 namespace Eppo\Exception;
 
+use Throwable;
+
 class InvalidApiKeyException extends EppoException
 {
+    public function __construct(
+        string $message = "Invalid API Key",
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
 }
