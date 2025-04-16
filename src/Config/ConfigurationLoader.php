6c4294586c85299f2fa608bde1bd40229181e11e
Ty Potter <tyler@fluxon.com>
2025-04-16 08:48:54 -0600

    comments

--- src/Config/ConfigurationLoader.php
+++ src/Config/ConfigurationLoader.php
@@ -51,10 +51,13 @@ class ConfigurationLoader
             }
             $fcr = FlagConfigResponse::fromArray($responseData);
             $banditResponse = null;
+            // If the flags reference Bandits, load bandits from the API, or reuse models already downloaded.
             if (count($fcr->banditReferences) > 0) {
+                // Assume we can reuse bandits.
                 $canReuseBandits = true;
                 $currentBandits = $currentConfig->getBanditModels();
 
+                // Check each referenced bandit model (what we need) against the current bandits (what we have).
                 foreach ($fcr->banditReferences as $banditKey => $banditReference) {
                     if (
                         !array_key_exists(
@@ -62,14 +65,18 @@ class ConfigurationLoader
                             $currentBandits
                         ) || $banditReference->modelVersion !== $currentBandits[$banditKey]
                     ) {
+                        // We don't have a bandit for this key or the model versions don't match.
                         $canReuseBandits = false;
                         break;
                     }
                 }
 
                 if ($canReuseBandits) {
+                    // Get the bandit ConfigResponse from the most recent configuration.
                     $banditResponse = $currentConfig->toConfigurationWire()->bandits;
                 } else {
+                    // Fetch the bandits from the API and build a ConfigResponse to populate the new
+                    // Configuration object.
                     $banditResource = $this->apiRequestWrapper->getBandits();
                     if (!$banditResource?->body) {
                         syslog(E_ERROR, "[Eppo SDK] Empty or invalid bandit response from the configuration server.");
